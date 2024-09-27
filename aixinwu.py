478cba367a005508cb935e9ae3a4cd66123e9b34
kody <tupobwq@foxmail.com>
2024-09-27 22:18:25 +0800

    发现之前的有问题，改一下

--- aixinwu.py
+++ aixinwu.py
@@ -1,76 +1,81 @@
-import json
-import os
-import pickle
-import re
+import logging
 from DrissionPage import ChromiumOptions, WebPage
 import requests
 from config import *
+
+logging.basicConfig(level=logging.INFO)
 
 AIXINWU_URL = "https://aixinwu.sjtu.edu.cn/user/order"
 ULOGIN_URL = "https://jaccount.sjtu.edu.cn/jaccount/ulogin"
 LAST_RESPONSE_HTML = "last_res"
 CAPTCHA_URL = "https://jaccount.sjtu.edu.cn/jaccount/captcha"
 
-def captcha_rec(captcha):
+def captcha_rec(img):
     try:
         files = {
-            'file': ('captcha.jpg', open(captcha, 'rb'), 'image/jpeg')
+            'file': ('captcha.jpg', img, 'image/jpeg')
         }
         req = requests.post('https://t.yctin.com/en/security/captcha-recognition/', files=files)
         text = req.text.strip()
-        print("验证码识别结果：", text)
+        logging.info(f"验证码识别结果：{text}")
         return text
     except Exception as e:
-        print(e)
+        logging.error(f"验证码识别失败: {e}")
         return None
 
-do = ChromiumOptions().set_paths(local_port=10000, user_data_path=r'.\data0', browser_path=BROWSER_PATH)
-
-# 用Edge浏览器时，需要先注释掉下面这行运行以界面模式运行一次，点一次脑抽的那个“我接受“，之后再取消注释，用headless模式运行
-# 其他使用有问题也可以注释掉这行自己研究研究
-do.headless()
-page = WebPage(chromium_options=do)
-page.get(AIXINWU_URL)
-
-attempts = 0
-while '403' in page.title and attempts < 5:
-    attempts += 1
-    login_btn = page.ele('.:ant-btn-primary')
-    login_btn.click()
-
+def login(page):
     username = page.ele('@id=input-login-user')
     username.input(USERNAME)
     password = page.ele('@id=input-login-pass')
     password.input(PASSWORD)
-    captcha_input = page.ele('@id=input-login-captcha')
+    attempts = 0
+    while '统一身份认证' in page.title and attempts < 5:
+        attempts += 1
+        if attempts >= 5:
+            logging.error("尝试失败次数过多，请检查密码或稍后再试")
+            page.save(name=LAST_RESPONSE_HTML)
+            page.close()
+            exit(1)
+        try:
+            captcha_input = page.ele('@id=input-login-captcha')
+            captcha_img = page.ele('@id=captcha-img')
+            img = captcha_img.get_screenshot(as_bytes=True, scroll_to_center=True)
+            with open('captcha.jpg', 'wb') as f:
+                f.write(img)
+            captcha_text = captcha_rec(img)
+            if captcha_text is None:
+                page.refresh()
+                logging.info("识别失败,正在重试")
+                continue
+            captcha_input.input(captcha_text,clear=True)
+            submit_btn = page.ele('@id=submit-password-button')
+            submit_btn.click()
+            page.wait(0.4)
+        except Exception as e:
+            logging.error(f"登录失败，第 {attempts} 次尝试: {e}")
+            page.refresh()
+            continue
 
-    page.wait(1)
-    captcha_img = page.ele('@id=captcha-img')
-    img = captcha_img.get_screenshot(as_bytes=True, scroll_to_center=True)
-    with open('captcha.jpg', 'wb') as f:
-        f.write(img)
+if __name__ == "__main__":
+    do = ChromiumOptions().set_paths(local_port=10000, user_data_path=r'.\data0', browser_path=BROWSER_PATH)
+    if HEADLESS:
+        do.headless()
+    page = WebPage(chromium_options=do)
+    page.get(AIXINWU_URL)
+    page.wait(0.4)
+    if "403" in page.title:
+        login_btn = page.ele('.:ant-btn-primary')
+        login_btn.click()
+        page.wait(0.45)
+        login(page)
+    try:
+        page.ele('.:anticon anticon-user').click()
+        page.wait(0.5)
+        label = page.ele('@text():已连续登录')
+        logging.info(label.text)
+    except Exception as e:
+        logging.error(f"登录天数未找到: {e}")
 
-    captcha_text = captcha_rec('captcha.jpg')
-    if captcha_text is None:
-        print("识别失败")
-        continue
-    captcha_input.input(captcha_text)
-    submit_btn = page.ele('@id=submit-password-button')
-    submit_btn.click()
-
-if attempts >= 5:
-    print("尝试失败次数过多，请稍后再试")
     page.save(name=LAST_RESPONSE_HTML)
+    logging.info(f"可查看 {LAST_RESPONSE_HTML}.mhtml 文件检查登录结果")
     page.close()
-    exit(1)
-
-page.save(name=LAST_RESPONSE_HTML)
-try:
-    page.ele('.:anticon anticon-user').click()
-    label = page.ele('@text():已连续登录')
-    print(label.text)
-except Exception as e:
-    print(e)
-    print("登录天数未找到，请查看last_res.mhtml文件检查登录结果")
-
-page.close()
--- config_template.py
+++ config_template.py
@@ -1,3 +1,4 @@
-USERNAME = 'YOUR_USERNAME',
+USERNAME = 'YOUR_USERNAME'
 PASSWORD = 'YOUR_PASSWORD'
-BROWSER_PATH = "C:\Program Files\Google\Chrome\Application\chrome.exe" # 改为你的浏览器路径，建议时chrome，edge的话看看aixinwu.py里的29行注释
+BROWSER_PATH = "C:\Program Files\Google\Chrome\Application\chrome.exe" # 改为你自己的浏览器路径，建议使用chrome，如果用edge的话第一次使用可能要先设置一次HEADLESS为False，点一次自动弹出的那个“我接受“
+HEADLESS = False # 为True则不会显示浏览器界面，如果程序存在问题，可以设为False显示浏览器界面检查
